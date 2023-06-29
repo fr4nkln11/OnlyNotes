f459859e6724320bb56d85a71c3ef47cac617c2a
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-06-29 21:17:58 +0100

    Create database tables for User and Notes

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "randomshit"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
+
     app.register_blueprint(views)
     app.register_blueprint(auth)
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
 
     return app
