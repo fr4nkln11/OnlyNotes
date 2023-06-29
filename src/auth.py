3ff04068ac1b6725b88255f93f42add8f0310ff4
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-06-29 20:58:36 +0100

    renamed register.html to sign_up.html, create form validation and flash alerts in signup page

--- src/auth.py
+++ src/auth.py
@@ -1,11 +1,60 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash
+)
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+def valid_password(password:str, password2: str) -> bool:
+    if password != password2:
+        flash("Passwords do not match", category="error")
+        return False
+    elif len(password) <= 7:
+        flash("Passwords must be at least 7 characters", category="error")
+        return False
+    else:
+        return True
+
+def valid_email(email:str) -> bool:
+    if '@' not in email:
+        return False
+    else:
+        return True
+
+def valid_name(name:str) -> bool:
+    if len(name) < 2:
+        return False
+    else:
+        return True
+
+def validate(email:str, name:str, password:str, password2:str):
+    if not valid_email(email):
+        flash("Invalid Email", category="error")
+        return False
+    elif not valid_name(name):
+        flash("Name cannot be less than 2 characters", category="error")
+        return False
+    elif not valid_password(password, password2):
+        return False
+    else:
+        return True
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
-@auth.route("/register")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        # get all data
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_confirm = request.form.get("password2")
+
+        if validate(email, username, password, password_confirm):
+            flash("User Account Succesfully created!", category="success")
+        
+    return render_template("sign_up.html") 
