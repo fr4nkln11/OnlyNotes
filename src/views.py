3596116c74efbfd86eab5b099982f41113a21668
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-06-30 12:58:17 +0100

    notes can be add to the database

--- src/views.py
+++ src/views.py
@@ -1,10 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import current_user, login_required
+from .models import Note
+from . import db
 
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
+def validNote(note):
+    if note != "":
+        flash("Note added succesfully!", category="success")
+        return True
+    else:
+        flash("Note cannot be empty", category="error")
+        return False
+
+
+@views.route("/", methods=["GET", "POST"])
 @login_required
 def home():
+    if request.method == "POST":
+        note = request.form.get("note")
+
+        if validNote(note):
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+
     return render_template("home.html", user=current_user)
