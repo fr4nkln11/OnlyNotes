b770df033e52fb0410589ec2470fb1be7120325a
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-06-30 13:47:57 +0100

    notes can now be deleted from the database

--- src/views.py
+++ src/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import current_user, login_required
 from .models import Note
 from . import db
+import json
 
 views = Blueprint("views", __name__)
 
@@ -27,3 +28,16 @@ def home():
             db.session.commit()
 
     return render_template("home.html", user=current_user)
+
+
+@views.route("/delete-note", methods=["POST"])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note["noteId"]
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
