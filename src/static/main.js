08e5110606343af616fdf61941f75733f32d70bb
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-05 14:47:04 +0100

    feature: Note cards can be opened as modals when clicked

--- src/static/main.js
+++ src/static/main.js
@@ -6,3 +6,15 @@ function deleteNote(noteId) {
     window.location.href = "/";
   });
 }
+
+function noteModal(noteId) {
+  let modalElement = document.querySelector("#noteViewModal")
+  let modalContent = document.querySelector("#noteViewModalContent")
+  let noteContent = document.querySelector(`#noteContent${noteId}`)
+  console.log(noteContent.textContent)
+  modalContent.textContent = noteContent.textContent
+  var noteModal = new bootstrap.Modal(modalElement, {
+    backdrop: true
+  })
+  noteModal.show()
+}
